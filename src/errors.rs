d34ed7c30e6782469ba70df327c71c65ba3fddd1
Speedy_Lex <alex.ciocildau@gmail.com>
2024-05-19 17:03:36 +0200

    Non panicking error messages
    requires nightly to build

---
+++ src/errors.rs
@@ -0,0 +1,38 @@
+use std::{fmt::Display, process::exit};
+
+use colored::Colorize;
+
+pub trait Expect<T, R, I> {
+    fn expect_np(self: Self, msg: &str) -> I;
+}
+impl<T, E> Expect<Result<T, E>, E, T> for Result<T, E> {
+    default fn expect_np(self: Self, msg: &str) -> T
+    {
+        match self {
+            Ok(t) => {
+                return t;
+            }
+            Err(_) => {
+                error(msg)
+            }
+        }
+    }
+}
+impl<T, E: Display> Expect<Result<T, E>, E, T> for Result<T, E> {
+    fn expect_np(self: Self, msg: &str) -> T
+    {
+        match self {
+            Ok(t) => {
+                return t;
+            }
+            Err(t) => {
+                error(&format!("{msg}\n{t}"))
+            }
+        }
+    }
+}
+
+pub fn error(msg: &str) -> ! {
+    eprintln!("{}{}", "ERROR: ".color("red"), msg.color("red"));
+    exit(1);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+#![feature(specialization)]
+#![allow(incomplete_features)]
+
+pub mod errors;
+
+use errors::{Expect, error};
+
 use std::env;
 use std::fs;
 use std::fs::File;
@@ -11,7 +18,7 @@ use json::JsonValue;
 fn main() {
     let args: Vec<_> = env::args().skip(1).collect();
     if args.len()==0 {
-        println!("commands\n\
+        println!("Commands:\n\
                 nasm_proj new <name> - makes a new project in the directory .\\<name>\n\
                 nasm_proj build - builds the project in the cwd into an exe\n\
                 nasm_proj run - builds and runs the project in the cwd\n\
@@ -27,38 +34,38 @@ fn main() {
             println!("project already exists");
             return;
         }
-        fs::create_dir_all(format!(".\\{}\\src", args[1])).expect("couldn't create directory tree");
-        File::create(format!(".\\{}\\src\\main.asm", args[1])).expect("couldn't make main.asm").write_all(include_bytes!("main.asm")).expect("couldn't write to main.asm");
-        File::create(format!(".\\{}\\nasm_proj.json", args[1])).expect("couldn't make project config").write_all(include_str!("nasm_proj.json").replace("$name", &args[1]).as_bytes()).expect("couldn't write to project config");
+        fs::create_dir_all(format!(".\\{}\\src", args[1])).expect_np("couldn't create directory tree");
+        File::create(format!(".\\{}\\src\\main.asm", args[1])).expect_np("couldn't make main.asm").write_all(include_bytes!("main.asm")).expect_np("couldn't write to main.asm");
+        File::create(format!(".\\{}\\nasm_proj.json", args[1])).expect_np("couldn't make project config").write_all(include_str!("nasm_proj.json").replace("$name", &args[1]).as_bytes()).expect_np("couldn't write to project config");
     } else if args[0]=="build" || args[0]=="b" {
         build();
     } else if args[0]=="run" || args[0]=="r" {
         build();
-        let mut p = env::current_dir().expect("cwd error");
+        let mut p = env::current_dir().expect_np("cwd error");
         let name = get_proj_name(json_config_open().as_ref());
         p.push(Path::new(&name));
         let mut c = Command::new(p.as_os_str());
-        c.spawn().unwrap_or_else(|x| {panic!("failed command: {}", x.to_string())}).wait().expect("couldn't wait for command");
-    } else if (args[0]=="clean" || args[0]=="c") && fs::read_dir(".\\build").is_ok() {
+        c.spawn().unwrap_or_else(|x| {error(&format!("command failed: {}", x))}).wait().expect_np("couldn't wait for command");
+    } else if args[0]=="clean" || args[0]=="c" {
         let name = get_proj_name(json_config_open().as_ref());
         if fs::read_dir(".\\build").is_ok() {
-            fs::remove_dir_all(".\\build").expect("couldn't delete build");
+            fs::remove_dir_all(".\\build").expect_np("couldn't delete build");
         }
         if File::open(format!(".\\{name}.exe")).is_ok() {
-            fs::remove_file(format!(".\\{name}.exe")).expect("couldn't delete exe");
+            fs::remove_file(format!(".\\{name}.exe")).expect_np("couldn't delete exe");
         }
     }
 }
 
 fn run_cmd(mut c: Command) {
-    if !c.spawn().unwrap_or_else(|x| {panic!("failed command: {}", x.to_string())}).wait().expect("couldn't wait for command").success() {
-        panic!("command failed");
+    if !c.spawn().unwrap_or_else(|x| {error(&format!("command failed: {}", x))}).wait().expect_np("couldn't wait for command").success() {
+        error("command failed");
     }
 }
 fn build() {
-    fs::create_dir_all(".\\build").expect("couldn't make build directory");
+    fs::create_dir_all(".\\build").expect_np("couldn't make build directory");
 
-    let j = json::parse(&fs::read_to_string(".\\nasm_proj.json").expect("couldn't read project config")).expect("couldn't parse json");
+    let j = json::parse(&fs::read_to_string(".\\nasm_proj.json").expect_np("couldn't read project config")).expect_np("couldn't parse json");
     let name = get_proj_name(json::Result::Ok(&j).as_deref());
     if let JsonValue::Object(s) = j {
         let build;
@@ -68,20 +75,20 @@ fn build() {
             build = b;
             link = l;
         } else {
-            panic!("JSON has wrong format");
+            error("JSON has wrong format");
         }
 
         println!("building");
         let mut objs = String::new();
-        for file in fs::read_dir(".\\src").expect("missing src directory") {
+        for file in fs::read_dir(".\\src").expect_np("missing src directory") {
             if let Ok(file) = file {
-                let fname = file.file_name().into_string().expect("non unicode filename");
+                let fname = file.file_name().into_string().expect_np("non unicode filename");
                 let (n, t) = fname.rsplit_once('.').or(Some((&fname, ""))).unwrap();
                 let t = format!(".{}", t);
                 objs.extend(format!("build\\\\{n}{t}.o ").chars());
 
                 if let (Ok(f), Ok(s)) = (File::open(&format!("build\\{n}{t}.o")), File::open(&format!("src\\{n}{t}"))) {
-                    if let (Ok(obj), Ok(src)) = (f.metadata().expect("couldn't get file metadata").modified(), s.metadata().expect("couldn't get file metadata").modified()) {
+                    if let (Ok(obj), Ok(src)) = (f.metadata().expect_np("couldn't get file metadata").modified(), s.metadata().expect_np("couldn't get file metadata").modified()) {
                         if src<obj {
                             continue;
                         }
@@ -97,7 +104,7 @@ fn build() {
                 }
 
             } else {
-                panic!("file error?");
+                error("file error?");
             }
         }
 
@@ -110,20 +117,20 @@ fn build() {
         let c = execute::command(c);
         run_cmd(c);
     } else {
-        panic!("JSON has wrong format");
+        error("JSON has wrong format");
     }
 }
 fn json_config_open() -> json::Result<JsonValue> {
-    json::parse(&fs::read_to_string(".\\nasm_proj.json").expect("couldn't read project config"))
+    json::parse(&fs::read_to_string(".\\nasm_proj.json").expect_np("couldn't read project config"))
 }
 fn get_proj_name(js: Result<&JsonValue, &json::JsonError>) -> String {
-    if let JsonValue::Object(s) = js.expect("couldn't parse json") {
+    if let JsonValue::Object(s) = js.expect_np("couldn't parse json") {
         if let Some(JsonValue::Short(l)) = s.get("name") {
             return l.to_string();
         } else {
-            panic!("JSON has wrong format");
+            error("JSON has wrong format");
         }
     } else {
-        panic!("JSON has wrong format");
+        error("JSON has wrong format");
     }
 }
